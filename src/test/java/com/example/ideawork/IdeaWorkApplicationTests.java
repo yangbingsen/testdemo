04ba0d811a34869a4747e01b31f85626d10c2b2c
yangbingsen <yang_bingsen@163.com>
2021-10-02 19:28:37 +0800

    Initial commit

---
+++ src/test/java/com/example/ideawork/IdeaWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideawork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
